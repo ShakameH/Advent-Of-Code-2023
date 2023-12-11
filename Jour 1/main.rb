f506a3a51df8d51d0d88256d47aa8621ccc397fd
Shakame <lucas.baujard1@gmail.com>
2023-12-11 21:39:16 +0100

    Initial commit

---
+++ Jour 1/main.rb
@@ -0,0 +1,37 @@
+class String
+    def is_integer
+        self.to_i.to_s == self
+    end
+end
+
+class FileReader
+    def initialize(file_path)
+        @file_path = file_path
+    end
+    def read_data
+        sum = 0
+        File.foreach(@file_path) do |line| 
+            first_number = nil
+            last_number = nil
+            line.each_char do |c|
+                if c.is_integer
+                    if first_number.nil?
+                        first_number = c
+                    else
+                        last_number = c
+                    end
+                end
+            end
+            if first_number && last_number
+                sum += (first_number + last_number).to_i
+            elsif first_number
+                sum += (first_number * 2).to_i
+            end
+        end
+        sum
+    end
+end
+
+file = FileReader.new("./input")
+sum = file.read_data
+puts sum
