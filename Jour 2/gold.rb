26f43227e540f1eb46990cd9f7542559715bf76c
Shakame <lucas.baujard1@gmail.com>
2023-12-13 12:34:04 +0100

    Day 2 - Gold Star

--- Jour 2/gold.rb
+++ Jour 2/gold.rb
@@ -1,10 +1,11 @@
 class Game
-    attr_reader :id, :bag
-    attr_writer :bag
+    attr_reader :id, :bag, :minimal_bag
+    attr_writer :bag, :minimal_bag
 
     def initialize(id)
         @id = id
         @bag = Bag.new
+        @minimal_bag = Bag.new
     end
 end
 
@@ -25,16 +26,15 @@ class Bag
 end
 
 class FileReader
-    attr_reader :sum_id
-    attr_writer :sum_id
+    attr_reader :sum_power
+    attr_writer :sum_power
     def initialize(file_path)
         @file_path = file_path
-        @sum_id = 0
+        @sum_power = 0
     end
 
     def read_file
         File.foreach(@file_path) do |line|
-            is_valid = true
             tirage_array =  line.split(/[:]/)
             game = Game.new(tirage_array[0].split(" ")[1])
 
@@ -43,20 +43,20 @@ class FileReader
                     number, color = y.split(" ")
                     game.bag.send("#{color}_cube=", game.bag.send("#{color}_cube") + number.to_i)
                 }
-                if game.bag.red_cube > 12 || game.bag.green_cube > 13 || game.bag.blue_cube > 14
-                    is_valid = false
-                    break
+                if game.bag.red_cube > game.minimal_bag.red_cube
+                    game.minimal_bag.red_cube = game.bag.red_cube
+                end
+                if game.bag.green_cube > game.minimal_bag.green_cube
+                    game.minimal_bag.green_cube = game.bag.green_cube
+                end
+                if game.bag.blue_cube > game.minimal_bag.blue_cube
+                    game.minimal_bag.blue_cube = game.bag.blue_cube
                 end
                 game.bag.clear_bag
-                if !is_valid
-                    break
-                end
             }
-            if is_valid
-                @sum_id += game.id.to_i
-            end
+        @sum_power += game.minimal_bag.red_cube * game.minimal_bag.green_cube * game.minimal_bag.blue_cube
         end
-        @sum_id
+        @sum_power
     end
 end
 
